1707f0fdc174816c3583c296cc112c2bc56852c9
Azad0645 <fenix16450@gmail.com>
2025-05-07 01:59:50 +0300

    Post data on the website

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,38 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils.timezone import localtime, now
+from datacenter.models import Passcard, Visit
+
+
+def format_duration(duration):
+    total_seconds = int(duration.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = get_object_or_404(Passcard, passcode=passcode)
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    visits = Visit.objects.filter(passcard=passcard)
+
+    this_passcard_visits = []
+    for visit in visits:
+        entered_at = localtime(visit.entered_at)
+        if visit.leaved_at:
+            duration = visit.leaved_at - visit.entered_at
+        else:
+            duration = now() - visit.entered_at
+
+        visit_info = {
+            'entered_at': entered_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'duration': format_duration(duration),
+            'is_strange': duration.total_seconds() > 3600
+        }
+        this_passcard_visits.append(visit_info)
+
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': this_passcard_visits,
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,32 @@
-from datacenter.models import Passcard
+from django.utils.timezone import localtime, now
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
-def storage_information_view(request):
-    # Программируем здесь
+def format_duration(duration):
+    total_seconds = int(duration.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+
+def storage_information_view(request):
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+
+    non_closed_visits = []
+    for visit in active_visits:
+        entered_at = localtime(visit.entered_at)
+        duration = now() - visit.entered_at
+        duration_str = format_duration(duration)
+
+        non_closed_visits.append({
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': entered_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'duration': duration_str,
+        })
+
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
